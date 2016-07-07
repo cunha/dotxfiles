39d465a615568d099759e0362e9cf6bd9fec2f57
Italo Cunha <cunha@dcc.ufmg.br>
2016-07-07 11:33:53 -0300

    fix swapintl on default xkb-data

--- bin/swapintl.sh
+++ bin/swapintl.sh
@@ -5,5 +5,5 @@ setxkbmap -query | grep intl > /dev/null
 if [ $? -eq 0 ] ; then
 	setxkbmap -layout us -option ctrl:swapcaps
 else
-	setxkbmap -layout us -variant intl -option ctrl:swapcaps
+	setxkbmap -layout us -variant intl -option "ctrl:swapcaps,lv3:ralt_alt"
 fi
