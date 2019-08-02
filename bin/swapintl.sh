8fbc74551bf911ab8c7c461b53ec247ce3360006
Italo Cunha <cunha@dcc.ufmg.br>
2019-08-02 11:15:07 -0400

    update

--- bin/swapintl.sh
+++ bin/swapintl.sh
@@ -4,12 +4,12 @@ set -u
 setxkbmap -query | grep intl > /dev/null
 if [ $? -eq 0 ] ; then
     setxkbmap -option
-    setxkbmap -layout us -option ctrl:swapcaps -option ctrl:swap_rwin_rctl
+    setxkbmap -layout us -option ctrl:nocaps -option ctrl:swap_rwin_rctl
     rm -f .cache/us-intl
     pkill -USR1 i3status
 else
     setxkbmap -option
-    setxkbmap -layout us -variant intl -option ctrl:swapcaps -option ctrl:swap_rwin_rctl
+    setxkbmap -layout us -variant intl -option ctrl:nocaps -option ctrl:swap_rwin_rctl
     touch .cache/us-intl
     pkill -USR1 i3status
 fi
