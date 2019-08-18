7697c6fde5bd4a06eccdd8a49c214860aa7528af
Italo Cunha <cunha@dcc.ufmg.br>
2019-08-18 13:18:27 -0400

    improvements to i3 and xkb

--- bin/swapintl.sh
+++ bin/swapintl.sh
@@ -4,12 +4,12 @@ set -u
 setxkbmap -query | grep intl > /dev/null
 if [ $? -eq 0 ] ; then
     setxkbmap -option
-    setxkbmap -layout us -option ctrl:nocaps -option ctrl:swap_rwin_rctl
+    setxkbmap -layout uscunha -option shift:both_capslock
     rm -f .cache/us-intl
     pkill -USR1 i3status
 else
     setxkbmap -option
-    setxkbmap -layout us -variant intl -option ctrl:nocaps -option ctrl:swap_rwin_rctl
+    setxkbmap -layout uscunha -variant alt-intl -option shift:both_capslock
     touch .cache/us-intl
     pkill -USR1 i3status
 fi
