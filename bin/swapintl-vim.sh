51fa984bd312b24fc4fe349d0561d28182c41a64
Italo Cunha <cunha@dcc.ufmg.br>
2020-02-26 16:00:44 -0300

    update keyboarding/terminal

--- bin/swapintl-vim.sh
+++ bin/swapintl-vim.sh
@@ -4,12 +4,12 @@ set -u
 setxkbmap -query | grep intl > /dev/null
 if [ $? -eq 0 ] ; then
     setxkbmap -option
-    setxkbmap -layout us -option shift:both_capslock -option ctrl:nocaps
+    setxkbmap -layout uscunha -option shift:both_capslock
     rm -f .cache/us-intl
     pkill -USR1 i3status
 else
     setxkbmap -option
-    setxkbmap -layout us -variant alt-intl -option shift:both_capslock -option ctrl:nocaps
+    setxkbmap -layout uscunha -variant alt-intl -option shift:both_capslock
     touch .cache/us-intl
     pkill -USR1 i3status
 fi
