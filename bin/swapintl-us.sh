aa4550cee47738395a040867546d0dd31ba6feb4
Italo Cunha <cunha@dcc.ufmg.br>
2020-03-03 16:24:01 -0300

    update

--- bin/swapintl-us.sh
+++ bin/swapintl-us.sh
@@ -4,12 +4,12 @@ set -u
 setxkbmap -query | grep intl > /dev/null
 if [ $? -eq 0 ] ; then
     setxkbmap -option
-    setxkbmap -layout uscunha -option shift:both_capslock
+    setxkbmap -layout us -option shift:both_capslock -option ctrl:nocaps
     rm -f .cache/us-intl
     pkill -USR1 i3status
 else
     setxkbmap -option
-    setxkbmap -layout uscunha -variant alt-intl -option shift:both_capslock
+    setxkbmap -layout us -variant alt-intl -option shift:both_capslock -option ctrl:nocaps
     touch .cache/us-intl
     pkill -USR1 i3status
 fi
